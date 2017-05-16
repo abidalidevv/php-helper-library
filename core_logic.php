22cb88c496f3a0939e5723f76e05bfe0c32a7140
abidmmp <abidmmp99@gmail.com>
2017-05-16 10:07:00 +0500

    Improve file upload validation

--- core_logic.php
+++ core_logic.php
@@ -104,3 +104,4 @@
 // Fix route parameter binding - 2017-05-03 10:05:00
 // Refactor service layer - 2024-07-03 10:01:00
 // Fix route parameter binding - 2019-06-01 10:11:00
+// Improve file upload validation - 2017-05-16 10:07:00
