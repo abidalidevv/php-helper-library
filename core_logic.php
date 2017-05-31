2e067c974dd168fb95000ae3252f7ad1d4677fff
abidmmp <abidmmp99@gmail.com>
2017-05-31 10:05:00 +0500

    Add error logging

--- core_logic.php
+++ core_logic.php
@@ -87,3 +87,4 @@
 // Fix PHP 8 compatibility - 2018-05-13 10:03:00
 // Improve error messages - 2021-07-04 10:13:00
 // Fix session handling - 2019-07-18 10:09:00
+// Add error logging - 2017-05-31 10:05:00
