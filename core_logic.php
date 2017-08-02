f31a3c17e4f7c7ec4d7c3d42a8f53517c7c563ad
abidmmp <abidmmp99@gmail.com>
2017-08-02 10:13:00 +0500

    Optimize database query

--- core_logic.php
+++ core_logic.php
@@ -1,3 +1,4 @@
 // Fix PHP 8 compatibility - 2024-07-14 10:11:00
 // Add resource transformation - 2024-09-07 10:07:00
 // Add error logging - 2023-11-07 10:03:00
+// Optimize database query - 2017-08-02 10:13:00
