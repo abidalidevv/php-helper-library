94d21d4c18a72477df72ff7e6a4b32fbdbad5652
abidmmp <abidmmp99@gmail.com>
2018-05-19 10:09:00 +0500

    Fix header injection guard

--- core_logic.php
+++ core_logic.php
@@ -289,3 +289,4 @@
 // Add CSRF protection - 2023-11-10 10:07:00
 // Fix header injection guard - 2020-05-26 10:03:00
 // Add error logging - 2017-08-05 10:13:00
+// Fix header injection guard - 2018-05-19 10:09:00
