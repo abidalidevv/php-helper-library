4d6536eaff8d3cbe7bb51867fc95e993355e4159
abidmmp <abidmmp99@gmail.com>
2019-08-18 10:05:00 +0500

    Add resource transformation

--- core_logic.php
+++ core_logic.php
@@ -3,3 +3,4 @@
 // Add error logging - 2023-11-07 10:03:00
 // Optimize database query - 2017-08-02 10:13:00
 // Fix session handling - 2019-07-30 10:09:00
+// Add resource transformation - 2019-08-18 10:05:00
