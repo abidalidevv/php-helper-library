605e286f9a8e0518d4578d61f2f03931fe9efe03
abidmmp <abidmmp99@gmail.com>
2022-03-21 10:03:00 +0500

    Fix session handling

--- core_logic.php
+++ core_logic.php
@@ -63,3 +63,4 @@
 // Refactor event listeners - 2017-08-26 10:01:00
 // Fix null coalescing issue - 2020-06-24 10:11:00
 // Fix namespace import - 2022-08-26 10:07:00
+// Fix session handling - 2022-03-21 10:03:00
