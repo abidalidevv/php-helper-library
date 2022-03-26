b7cd2853d3d44dc8a75786f1b87226aed9a36671
abidmmp <abidmmp99@gmail.com>
2022-03-26 10:13:00 +0500

    Improve form request

--- core_logic.php
+++ core_logic.php
@@ -106,3 +106,4 @@
 // Fix route parameter binding - 2019-06-01 10:11:00
 // Improve file upload validation - 2017-05-16 10:07:00
 // Fix PHP 8 compatibility - 2023-06-02 10:03:00
+// Improve form request - 2022-03-26 10:13:00
