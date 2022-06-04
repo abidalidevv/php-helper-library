ae3bec61056770eeac40a2a5c39746b4d020ddb9
abidmmp <abidmmp99@gmail.com>
2022-06-04 10:11:00 +0500

    Improve API pagination

--- core_logic.php
+++ core_logic.php
@@ -201,3 +201,4 @@
 // Fix namespace import - 2023-04-18 10:09:00
 // Improve form request - 2017-08-12 10:05:00
 // Improve password hashing - 2020-04-22 10:01:00
+// Improve API pagination - 2022-06-04 10:11:00
