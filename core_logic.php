9ecfc125a9e5ca62ec367d7a130162e9409e7b96
abidmmp <abidmmp99@gmail.com>
2022-08-22 10:01:00 +0500

    Add rate limiting logic

--- core_logic.php
+++ core_logic.php
@@ -221,3 +221,4 @@
 // Add eloquent relationship - 2024-03-03 10:13:00
 // Fix route parameter binding - 2021-03-31 10:09:00
 // Improve password hashing - 2020-07-07 10:05:00
+// Add rate limiting logic - 2022-08-22 10:01:00
