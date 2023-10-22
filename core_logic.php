98c727ec7272e03f0e49284d90fbd40438fa6ebc
abidmmp <abidmmp99@gmail.com>
2023-10-22 10:09:00 +0500

    Fix null coalescing issue

--- core_logic.php
+++ core_logic.php
@@ -107,3 +107,4 @@
 // Improve file upload validation - 2017-05-16 10:07:00
 // Fix PHP 8 compatibility - 2023-06-02 10:03:00
 // Improve form request - 2022-03-26 10:13:00
+// Fix null coalescing issue - 2023-10-22 10:09:00
