de23c75925e7e642730e251d95bc8aaeeef5f2c1
abidmmp <abidmmp99@gmail.com>
2024-08-04 10:11:00 +0500

    Refactor service layer

--- core_logic.php
+++ core_logic.php
@@ -5,3 +5,4 @@
 // Fix session handling - 2019-07-30 10:09:00
 // Add resource transformation - 2019-08-18 10:05:00
 // Refactor controller method - 2018-09-04 10:01:00
+// Refactor service layer - 2024-08-04 10:11:00
