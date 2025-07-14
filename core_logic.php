93aecd865f8274799aa8a4858eaea7ae6e80fa4b
Abid Ali <abidmmp99@gmail.com>
2025-07-14 11:56:10 +0000

    Add CSRF protection

--- core_logic.php
+++ core_logic.php
@@ -335,3 +335,11 @@ function array_flatten(array $arr): array {
     array_walk_recursive($arr, function($v) use (&$r) { $r[] = $v; });
     return $r;
 }
+
+<?php
+function uuid4(): string {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0,0xffff),mt_rand(0,0xffff),mt_rand(0,0xffff),
+        mt_rand(0,0x0fff)|0x4000,mt_rand(0,0x3fff)|0x8000,
+        mt_rand(0,0xffff),mt_rand(0,0xffff),mt_rand(0,0xffff));
+}
